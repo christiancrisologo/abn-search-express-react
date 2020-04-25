f660f987b18516629cabf29c2ac3c808577444c7
ChristianCrisologo <christian.crisologo@gmail.com>
2020-04-25 21:50:31 +1000

    add build for production

---
+++ client/build/precache-manifest.4b5a85b9390969e53490ee767e15392e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3840497398ca57c89605cb8a0c2cef90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24d560219b16dc5a31ca",
+    "url": "/static/js/2.fd3408d8.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.fd3408d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af22425daa932533b3d",
+    "url": "/static/js/main.03c0ed64.chunk.js"
+  },
+  {
+    "revision": "43b335b64dc334bdf58f",
+    "url": "/static/js/runtime-main.f0e26e7c.js"
+  }
+]);
