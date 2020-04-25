963dec7b53610ca877694b51100954c6c4a1f16b
ChristianCrisologo <christian.crisologo@gmail.com>
2020-04-25 22:30:27 +1000

    fixing the port issue in heroku , add logs on client

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { ACTION_TYPES } from "./";
-const END_POINT = `${window.location.protocol}//${window.location.hostname}:${process.env.PORT}`;
+const END_POINT = `${window.location.protocol}//${window.location.hostname}:${
+  process.env.PORT || 4000
+}`;
 
 const asyncRequest = async function fetchAsync(url, options = {}) {
+  console.log("#CC making a request at ", url);
   const response = await axios({
     url,
     method: options.method || "get",
