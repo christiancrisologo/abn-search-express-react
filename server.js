3732d8f0a1f2b19c127d2df7a2e757340bf3ff15
ChristianCrisologo <christian.crisologo@gmail.com>
2020-04-25 21:46:24 +1000

    add server script to handle the ABR apis

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// for local dev not for production
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+function send(res, result, status = 200) {
+  res.status(status);
+  res.send(result);
+}
+
+const ABR_URL = "https://abr.business.gov.au/json";
+const GUID = process.env.ABR_GUID;
+
+app.get("/abn/:key", async (req, res) => {
+  const response = await fetch(
+    `${ABR_URL}/AbnDetails.aspx?abn=${req.params.key}&callback=callback&guid=${GUID}`
+  );
+  const contents = await response.text();
+  const result = JSON.parse(
+    contents.substring("callback(".length, contents.length - 1)
+  );
+  send(res, result);
+});
+
+app.get("/company/:key", async (req, res) => {
+  const response = await fetch(
+    `${ABR_URL}/MatchingNames.aspx?name=${req.params.key}&maxResults=20&callback=callback&guid=${GUID}`
+  );
+  const contents = await response.text();
+  const result = JSON.parse(
+    contents.substring("callback(".length, contents.length - 1)
+  );
+  send(res, result);
+});
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log("App is listening on port " + port);
